61576e6967b098d24747bd08c3d7ac9d3bdc02b1
orestznak <orestznak@gmail.com>
2024-02-25 15:11:09 +0200

    2

--- angular-crash/src/app/app.component.ts
+++ angular-crash/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IProduct } from './models/product';
+
+import {produts as data} from './data/products'
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,9 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'angular-crash';
+  title = 'angular-crash'
+
+  products: IProduct[] = data
 }
