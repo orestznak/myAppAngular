13ecc6dfd913d34aa78726cca6a6bbadb5a25645
orestznak <orestznak@gmail.com>
2024-03-06 23:41:20 +0200

    отримання даних

--- angular-crash/src/app/app.component.ts
+++ angular-crash/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
 import { IProduct } from './models/product';
 import { ProductComponent } from './components/product/product.component';
 import { CommonModule } from '@angular/common';
-import { ProductService } from './services/product.services';
+import { ProductsService } from './services/products.service';
 
 
 @Component({
@@ -23,14 +23,16 @@ export class AppComponent implements OnInit{
 
   products: IProduct[] = []
 
-  constructor(private productsService: ProductService){
-          }
+  constructor(private productsService: ProductsService){
+
+  }
 
   ngOnInit(): void {
-    this.productsService.getAll().subscribe((products) => {
+    this.productsService.getAll().subscribe(next: products => {
       console.log(products)
     })
   
   }
 
 }
+
---
+++ angular-crash/src/app/services/products.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+
+import { Observable } from "rxjs";
+import { IProduct } from "../models/product";
+
+
+
+@Injectable({
+    providedIn:'root'
+})
+
+export class ProductsService{
+    
+ 
+    constructor(private http: HttpClient){
+
+    }
+    
+
+    getAll(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>( 'https://fakestoreapi.com/products')
+                    
+    }
+    
+}
